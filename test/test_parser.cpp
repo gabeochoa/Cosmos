d5a7c8aaddead542b42bccb4308a63b60d7ed939
Gabe <gabe1118@yahoo.com>
2016-06-14 19:22:31 -0400

    write simple parser validation test

--- test/test_parser.cpp
+++ test/test_parser.cpp
@@ -4,43 +4,93 @@
 #include <string>
 #include <sstream>
 
-class FooTest : public ::testing::Test {
- protected:
-  // You can remove any or all of the following functions if its body
-  // is empty.
+class ParserTest : public ::testing::Test {
+    protected:
+    // You can remove any or all of the following functions if its body
+    // is empty.
+    std::string errstr;
+    std::string inputstr;
+    std::vector<std::string> outputvec;
 
-  FooTest() {
+    ParserTest() {
     // You can do set-up work for each test here.
-  }
 
-  virtual ~FooTest() {
+    }
+
+    virtual ~ParserTest() {
     // You can do clean-up work that doesn't throw exceptions here.
-  }
+    }
 
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
 
-  virtual void SetUp() {
+    virtual void SetUp() {
     // Code here will be called immediately after the constructor (right
     // before each test).
-  }
+    }
 
-  virtual void TearDown() {
+    virtual void TearDown() {
     // Code here will be called immediately after each test (right
     // before the destructor).
-  }
+        errstr.clear();
+        inputstr.clear();
+        outputvec.clear();
+    }
 
-  // Objects declared here can be used by all tests in the test case for Foo.
 };
 
 
-
-TEST(FooTest, MethodBarDoesAbc) {
-    std::stringstream ss1("DB POST SELL OIL k 1 10.5");
-    std::stringstream ss2("DB REVOKE k 1");
-    std::stringstream ss3("DB CHECK + 1");
-    std::stringstream ss4("DB AGGRESS + 1 10");
-    std::stringstream ss5("DB AGGRESS 1");
-    std::stringstream ss6("DB REVOKE");
-    std::stringstream ss7("");
+//bool isValid(std::string&, std::string&, std::vector<std::string>&);  
+TEST_F(ParserTest, FalseOnBadDealer) {
+    Parser parser;
+    inputstr = "BADDEALER POST BUY GOLD 12 34.56";
+    bool outcome = parser.isValid(errstr, inputstr, outputvec);
+    ASSERT_FALSE(outcome) << "output string = " << ::testing::PrintToString(outputvec);
 }
+TEST_F(ParserTest, FalseOnBadCommand) {
+    Parser parser;
+    inputstr = "DB BADCOMMAND BUY GOLD 12 34.56";
+    bool outcome = parser.isValid(errstr, inputstr, outputvec);
+    ASSERT_FALSE(outcome) << "output string = " << ::testing::PrintToString(outputvec);
+}
+TEST_F(ParserTest, FalseOnBadSide) {
+    Parser parser;
+    inputstr = "DB POST BADSIDE GOLD 12 34.56";
+    bool outcome = parser.isValid(errstr, inputstr, outputvec);
+    ASSERT_FALSE(outcome) << "output string = " << ::testing::PrintToString(outputvec);
+}
+TEST_F(ParserTest, FalseOnBadCommodity) {
+    Parser parser;
+    inputstr = "DB POST BUY BADCOM 12 34.56";
+    bool outcome = parser.isValid(errstr, inputstr, outputvec);
+    ASSERT_FALSE(outcome) << "output string = " << ::testing::PrintToString(outputvec);
+}
+TEST_F(ParserTest, FalseOnBadQuantityNAN) {
+    Parser parser;
+    inputstr = "DB POST BUY GOLD NAN 34.56";
+    bool outcome = parser.isValid(errstr, inputstr, outputvec);
+    ASSERT_FALSE(outcome) << "output string = " << ::testing::PrintToString(outputvec);
+}
+TEST_F(ParserTest, FalseOnBadQuantityNotAnInt) {
+    Parser parser;
+    inputstr = "DB POST BUY GOLD 12.02 34.56";
+    bool outcome = parser.isValid(errstr, inputstr, outputvec);
+    ASSERT_FALSE(outcome) << "output string = " << ::testing::PrintToString(outputvec);
+}
+TEST_F(ParserTest, FalseOnBadPriceNAN) {
+    Parser parser;
+    inputstr = "DB POST BUY GOLD 12 NAN";
+    bool outcome = parser.isValid(errstr, inputstr, outputvec);
+    ASSERT_FALSE(outcome) << "output string = " << ::testing::PrintToString(outputvec);
+}
+
+
+
+
+
+
+
+
+
+
+
